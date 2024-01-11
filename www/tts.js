5b5e696f79e6fe7f4b094d6bbe3728eb019060b7
Justin Go <76882639+oryango@users.noreply.github.com>
2024-01-12 04:12:03 +0800

    Add files via upload

---
+++ www/tts.js
@@ -0,0 +1,62 @@
+/*
+
+    Cordova Text-to-Speech Plugin
+    https://github.com/vilic/cordova-plugin-tts
+
+    by VILIC VANE
+    https://github.com/vilic
+
+    MIT License
+
+*/
+
+exports.speak = function (text) {
+    const start = new Date().valueOf()
+    var options = {};
+    if (typeof text == "string") {
+      options.text = text;
+    } else {
+      options = text;
+    }
+    
+    cordova.exec((result)=>{
+        const now = new Date().valueOf();
+        if(result.type === "Word Boundary") {
+            console.log({
+                ...result,
+                elapsedTime: now - start,
+                currentWord: text.substr(result.charIndex, result.charLen),
+            });
+        } else {
+            console.log({
+                ...result,
+                elapsedTime: now - start,
+            });
+        }
+        
+    }, null, "TTS", "speak", [options]);
+};
+
+exports.stop = function () {
+  return new Promise(function (resolve, reject) {
+    cordova.exec(resolve, reject, "TTS", "stop", []);
+  });
+};
+
+exports.checkLanguage = function () {
+  return new Promise(function (resolve, reject) {
+    cordova.exec(resolve, reject, "TTS", "checkLanguage", []);
+  });
+};
+
+exports.getVoices = function () {
+  return new Promise(function (resolve, reject) {
+    cordova.exec(resolve, reject, "TTS", "getVoices", []);
+  });
+};
+
+exports.openInstallTts = function () {
+  return new Promise(function (resolve, reject) {
+    cordova.exec(resolve, reject, "TTS", "openInstallTts", []);
+  });
+};
